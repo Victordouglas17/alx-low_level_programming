6a55e917b0415f37ecb60bd5c563cf09aa72eb73
victor Douglas <douglasvictor266@gmail.com>
2022-10-20 04:33:45 +0000

    Victor Douglas

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
+/**
+ * Main Entry
+ * Return: always 
+ */
+
 int main(void)
 {
-	/**
-	 * Main Entry Point
-	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
