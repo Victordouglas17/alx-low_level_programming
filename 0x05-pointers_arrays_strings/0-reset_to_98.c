859254e401e4d283b4c3137506179e6dc5d05a76
victor Douglas <douglasvictor266@gmail.com>
2022-10-24 16:49:59 +0000

    C point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Resets a value to 98
+* @n: input resets value to 98
+* Return: void.
+*/
+
+void reset_to_98 (int *n)
+{
+	int *n;
+	*n = 98;
+	printf("Value of pointer *n: %x\n" *n);
+}
