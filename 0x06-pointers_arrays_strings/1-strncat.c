8b9208ffd3cb63e5ff0a9caa663d4c8e5779363c
victor Douglas <douglasvictor266@gmail.com>
2022-10-26 21:27:58 +0000

    C victor

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenate two string
+ * @src: input string
+ * @dest: input string
+ * @n: input int
+ * Return: void.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
